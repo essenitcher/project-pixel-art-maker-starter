66ccad39b84c70691aa848d3e36d5fa9ecdcd602
Mauro Galvan Rainhart <mauro.galvan@globant.com>
2017-12-06 16:15:18 -0300

    Formatted js file

--- designs.js
+++ designs.js
@@ -1,42 +1,40 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 
 function makeGrid() {
 
- //Draw a table
- var rows, cols, table;
- 
- rows = $("#input_height").val();
- cols = $("#input_width").val();
+	//Draw a table
+	var rows, cols, table;
 
- //get the table
- table = $("#pixel_canvas");
- 
- //empty the table
- table.empty();
- 
- for(var r = 0; r < rows; r++){
-	 table.append("<tr id=\"r" + r + "\"></tr>");
-	 var curRow = $("#r" + r);
-	 //Add row
-	 for(var c = 0; c < cols; c++){
-		 //add column
-		  curRow.append("<td id=\"r" + r + "-" +c + "\"></td>");
-	 }
- }
+	rows = $("#input_height").val();
+	cols = $("#input_width").val();
+
+	//get the table
+	table = $("#pixel_canvas");
+
+	//empty the table
+	table.empty();
+
+	for(var r = 0; r < rows; r++){
+		table.append("<tr id=\"r" + r + "\"></tr>");
+		var curRow = $("#r" + r);
+		
+		//Add row
+		for(var c = 0; c < cols; c++){
+			//add column
+			curRow.append("<td id=\"r" + r + "-" +c + "\"></td>");
+		}
+	}
 }
 
 function paintSquare(){
+	//Getting color
 	var color = $("#colorPicker").val();
+	//Applying it to the TD
 	$(this).css("background-color", color);
-
 }
 
 
 $(function(){
+	//Adding listeners to events
 	$("#submit_button").click(makeGrid);
 	$("#pixel_canvas").on('click', 'td', paintSquare);
 });
